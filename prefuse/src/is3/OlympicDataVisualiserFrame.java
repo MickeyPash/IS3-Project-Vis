52d5fa5c304e635f8ac440876c6a1a3686c4cdcb
Michael McDonald <mcdonaldm1993@gmail.com>
2013-12-02 22:55:14 +0000

    Added comments in the visualiser frame

--- prefuse/src/is3/OlympicDataVisualiserFrame.java
+++ prefuse/src/is3/OlympicDataVisualiserFrame.java
@@ -23,8 +23,14 @@ import prefuse.demos.ScatterPlot;
  */
 public class OlympicDataVisualiserFrame extends JFrame {
 
+	// This is the location of the data, This is passed into the constructor of
+	// every visualisation
 	private String data = "/WrangledData.csv";
+	// This is a String array of the names of visualisations. These names must
+	// match the name of the visualisation class (whitespace is allowed)
 	private String[] visualisationOptions = { "Olympics" };
+	// This is the array the visualisations will be added to when the program
+	// runs. Each visualisation will then be hidden/shown
 	private ArrayList<Display> visualisations = new ArrayList<Display>();
 
 	/**
@@ -32,11 +38,16 @@ public class OlympicDataVisualiserFrame extends JFrame {
 	 */
 	public OlympicDataVisualiserFrame() {
 
+		// This instantiates the classes based of the name in the
+		// visualisationOptions array
+		// Each of these classes must have a constructor which takes one
+		// string(data) to open the csv file, else the program will exit
 		for (String visualisation : visualisationOptions) {
 			Class<?> clazz;
 			Constructor<?> constructor;
 			try {
-				clazz = Class.forName("is3."+visualisation.replaceAll("\\s+", ""));
+				clazz = Class.forName("is3."
+						+ visualisation.replaceAll("\\s+", ""));
 				constructor = clazz.getConstructor(String.class);
 				visualisations.add((Display) constructor.newInstance(data));
 			} catch (ClassNotFoundException | NoSuchMethodException
@@ -48,23 +59,30 @@ public class OlympicDataVisualiserFrame extends JFrame {
 			}
 		}
 
+		// Setting up the combo box and panel
 		setTitle("Olympic Data Visualiser");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 		JComboBox<String> visualisationSelector = new JComboBox<String>(
 				visualisationOptions);
+		visualisationSelector.addActionListener(new ComboBoxListener());
 
 		getContentPane().add(visualisationSelector, BorderLayout.PAGE_START);
-		getContentPane().add(
-				ScatterPlot.demo("/fisher.iris.txt", "SepalLength",
-						"PetalLength"), BorderLayout.CENTER);
+
+		// This was just to test that a display could be shown/hidden in the
+		// right spot
+//		getContentPane().add(
+//				ScatterPlot.demo("/fisher.iris.txt", "SepalLength",
+//						"PetalLength"), BorderLayout.CENTER);
 
 		pack();
-		setVisible(true);	
+		setVisible(true);
 	}
 
 	private class ComboBoxListener implements ActionListener {
 
+		// This inner class will receive events triggered by the combo box
+		// changing and will hide/show the visualisations appropriately
 		@SuppressWarnings("rawtypes")
 		@Override
 		public void actionPerformed(ActionEvent e) {
@@ -80,5 +98,5 @@ public class OlympicDataVisualiserFrame extends JFrame {
 		}
 
 	}
-	
+
 }
