21b7c55f412f0756140f414a99e8127d092f7061
Michael McDonald <mcdonaldm1993@gmail.com>
2013-12-03 00:34:15 +0000

    Made scatterplot view update infinitely

--- prefuse/src/is3/OlympicScatterplot.java
+++ prefuse/src/is3/OlympicScatterplot.java
@@ -11,9 +11,11 @@ import prefuse.Constants;
 import prefuse.Display;
 import prefuse.Visualization;
 import prefuse.action.ActionList;
+import prefuse.action.RepaintAction;
 import prefuse.action.assignment.DataColorAction;
 import prefuse.action.layout.AxisLabelLayout;
 import prefuse.action.layout.AxisLayout;
+import prefuse.activity.Activity;
 import prefuse.controls.PanControl;
 import prefuse.controls.ToolTipControl;
 import prefuse.controls.ZoomControl;
@@ -31,23 +33,23 @@ import prefuse.visual.expression.VisiblePredicate;
 import prefuse.visual.sort.ItemSorter;
 
 public class OlympicScatterplot extends Display {
-	
+
 	private Table table;
 	private AxisLayout x_axis;
 	private AxisLayout y_axis;
-	
+
 	public OlympicScatterplot(String csvfile) {
-		
+
 		// Help mainly found at:
 		// http://www.ifs.tuwien.ac.at/~rind/w/doku.php/java/prefuse-scatterplot
-		
+
 		super(new Visualization());
-	    
+
 		table = new Table();
 		Schema schema = new Schema();
-		
+
 		// Schema allows specifying type of data being stored
-		
+
 		schema.addColumn("Country", String.class);
 		schema.addColumn("Continent", int.class);
 		schema.addColumn("Adult literacy rate (%)", Double.class);
@@ -62,113 +64,123 @@ public class OlympicScatterplot extends Display {
 		schema.addColumn("Gold", int.class);
 		schema.addColumn("Silver", int.class);
 		schema.addColumn("Bronze", int.class);
-		
+
 		table.addColumns(schema);
-		
-		/* this is me just randomly putting 200 ssince I know there isnt more than 200 countries, need to change this */
+
+		/*
+		 * this is me just randomly putting 200 since I know there isn't more
+		 * than 200 countries, need to change this
+		 */
 		table.addRows(200);
-		
+
 		// Read table from csv file
 		try {
 			table = new CSVTableReader().readTable(csvfile);
-		}
-		catch (DataIOException e) {
+		} catch (DataIOException e) {
 			e.printStackTrace();
-	        System.err.println("Error loading table. Exiting...");
-	        System.exit(1);
+			System.err.println("Error loading table. Exiting...");
+			System.exit(1);
 		}
-		
+
 		final Rectangle2D boundsData = new Rectangle2D.Double();
 		final Rectangle2D boundsLabelsX = new Rectangle2D.Double();
 		final Rectangle2D boundsLabelsY = new Rectangle2D.Double();
-	    
+
 		// ------------------------------------------------------------------
 		// Step 1: setup the visualised data
-		
+
 		VisualTable vt = m_vis.addTable("data", table);
-		
+
 		vt.addColumn("label", "CONCAT([Country],': ', ' Gold: ', [Gold], ';  Silver: ', [Silver], ';  Bronze: ', [Bronze])");
-		
+
 		// ------------------------------------------------------------------
 		// Step 2: setup renderers for visualised data
-		
-		DefaultRendererFactory rf = new DefaultRendererFactory(); 
-		
+
+		DefaultRendererFactory rf = new DefaultRendererFactory();
+
 		// Render labels on x & y axis
-		rf.add(new InGroupPredicate("ylab"), 
-				new AxisRenderer(Constants.FAR_LEFT, Constants.CENTER));
-		rf.add(new InGroupPredicate("xlab"), 
-				new AxisRenderer(Constants.CENTER, Constants.FAR_BOTTOM));
+		rf.add(new InGroupPredicate("ylab"), new AxisRenderer(
+				Constants.FAR_LEFT, Constants.CENTER));
+		rf.add(new InGroupPredicate("xlab"), new AxisRenderer(Constants.CENTER,
+				Constants.FAR_BOTTOM));
 		m_vis.setRendererFactory(rf);
-		
+
 		// ------------------------------------------------------------------
 		// Step 3: create actions to process the visual data
-		
-		x_axis = new AxisLayout("data", "TeamSize", Constants.X_AXIS, VisiblePredicate.TRUE);
-		y_axis = new AxisLayout("data", "Gold", Constants.Y_AXIS, VisiblePredicate.TRUE);
-		
+
+		x_axis = new AxisLayout("data", "TeamSize", Constants.X_AXIS,
+				VisiblePredicate.TRUE);
+		y_axis = new AxisLayout("data", "Gold", Constants.Y_AXIS,
+				VisiblePredicate.TRUE);
+
 		x_axis.setLayoutBounds(boundsData);
 		y_axis.setLayoutBounds(boundsData);
-		
-		AxisLabelLayout x_labels = new AxisLabelLayout("xlab", x_axis, boundsLabelsX);
-		AxisLabelLayout y_labels = new AxisLabelLayout("ylab", y_axis, boundsLabelsY);
-		
+
+		AxisLabelLayout x_labels = new AxisLabelLayout("xlab", x_axis,
+				boundsLabelsX);
+		AxisLabelLayout y_labels = new AxisLabelLayout("ylab", y_axis,
+				boundsLabelsY);
+
 		// Colour palette for continents
 		int[] palette = new int[] {
 				// Middle East
-	            ColorLib.rgba(20,120,100,125),
-	            // Europe
-	            ColorLib.rgba(105,245,40,125),
-	            // Africa
-	            ColorLib.rgba(200,200,135,125),
-	            // North America
-	            ColorLib.rgba(35,50,225,125),
-	            // South America
-	            ColorLib.rgba(190,115,20,125),
-	            // Oceania & some Asia
-	            ColorLib.rgba(95,175,210,125),
-	            // Asia
-	            ColorLib.rgba(220,15,40,125)
-	        };
-		
-		DataColorAction fill = new DataColorAction("data", "Continent", Constants.NOMINAL, VisualItem.FILLCOLOR, palette);
-		
-		ActionList draw = new ActionList();
+				ColorLib.rgba(20, 120, 100, 125),
+				// Europe
+				ColorLib.rgba(105, 245, 40, 125),
+				// Africa
+				ColorLib.rgba(200, 200, 135, 125),
+				// North America
+				ColorLib.rgba(35, 50, 225, 125),
+				// South America
+				ColorLib.rgba(190, 115, 20, 125),
+				// Oceania & some Asia
+				ColorLib.rgba(95, 175, 210, 125),
+				// Asia
+				ColorLib.rgba(220, 15, 40, 125) };
+
+		DataColorAction fill = new DataColorAction("data", "Continent",
+				Constants.NOMINAL, VisualItem.FILLCOLOR, palette);
+
+		ActionList draw = new ActionList(Activity.INFINITY);
 		draw.add(x_axis);
 		draw.add(y_axis);
 		draw.add(x_labels);
 		draw.add(y_labels);
 		draw.add(fill);
-		// draw.add(new RepaintAction());
+		draw.add(new RepaintAction());
 		m_vis.putAction("draw", draw);
-		
-		ActionList update = new ActionList();
-		update.add(x_axis);
-		update.add(y_axis);
-		update.add(x_labels);
-		update.add(y_labels);
-		update.add(fill);
-		// draw.add(new RepaintAction());
-		m_vis.putAction("update", update);
+
+		// Dont think this is needed anymore. Now that the draw ActionList will
+		// run for an infinite amount of time(in the argument in the actionlist
+		// constructor) and the actionlist has the RepaintAction in it which
+		// tells it to call this actionlist any time the data is updated
+//		ActionList update = new ActionList();
+//		update.add(x_axis);
+//		update.add(y_axis);
+//		update.add(x_labels);
+//		update.add(y_labels);
+//		update.add(fill);
+//		update.add(new RepaintAction());
+//		m_vis.putAction("update", update);
 
 		// ------------------------------------------------------------------
 		// Step 4: Setup a display and controls
 
 		// set display size
 		setSize(720, 500);
-	    // pan with left-click drag on background
-	    addControlListener(new PanControl()); 
-	    // zoom with right-click drag
-	    addControlListener(new ZoomControl());
-	    
-	    // Update visuals when user resizes display
-	    addComponentListener(new ComponentAdapter() {
+		// pan with left-click drag on background
+		addControlListener(new PanControl());
+		// zoom with right-click drag
+		addControlListener(new ZoomControl());
+
+		// Update visuals when user resizes display
+		addComponentListener(new ComponentAdapter() {
 			public void componentResized(ComponentEvent e) {
 				updateBounds(boundsData, boundsLabelsX, boundsLabelsY);
 				m_vis.run("update");
 			}
 		});
-		
+
 		setItemSorter(new ItemSorter() {
 			public int score(VisualItem item) {
 				int score = super.score(item);
@@ -177,27 +189,29 @@ public class OlympicScatterplot extends Display {
 				return score;
 			}
 		});
-		 
-	    // Display specific information when hovering over point
+
+		// Display specific information when hovering over point
 		ToolTipControl ttc = new ToolTipControl("label");
 		addControlListener(ttc);
-		
+
 		// ------------------------------------------------------------------
 		// Step 5: Launching the visualisation
 
 		updateBounds(boundsData, boundsLabelsX, boundsLabelsY);
 		m_vis.run("draw");
-		
+
 	}
-	
+
 	/**
-	 * Taken from http://www.ifs.tuwien.ac.at/~rind/w/doku.php/java/prefuse-scatterplot
+	 * Taken from
+	 * http://www.ifs.tuwien.ac.at/~rind/w/doku.php/java/prefuse-scatterplot
 	 * 
 	 * @param boundsData
 	 * @param boundsLabelsX
 	 * @param boundsLabelsY
 	 */
-	private void updateBounds(Rectangle2D boundsData, Rectangle2D boundsLabelsX, Rectangle2D boundsLabelsY) {
+	private void updateBounds(Rectangle2D boundsData,
+			Rectangle2D boundsLabelsX, Rectangle2D boundsLabelsY) {
 
 		int paddingLeft = 30;
 		int paddingTop = 15;
@@ -211,8 +225,10 @@ public class OlympicScatterplot extends Display {
 
 		int left = i.left + paddingLeft;
 		int top = i.top + paddingTop;
-		int innerWidth = getWidth() - i.left - i.right - paddingLeft - paddingRight;
-		int innerHeight = getHeight() - i.top - i.bottom - paddingTop - paddingBottom;
+		int innerWidth = getWidth() - i.left - i.right - paddingLeft
+				- paddingRight;
+		int innerHeight = getHeight() - i.top - i.bottom - paddingTop
+				- paddingBottom;
 
 		boundsData.setRect(left + axisWidth, top, innerWidth - axisWidth,
 				innerHeight - axisHeight);
@@ -221,9 +237,10 @@ public class OlympicScatterplot extends Display {
 		boundsLabelsY.setRect(left, top, innerWidth + paddingRight, innerHeight
 				- axisHeight);
 	}
-	
+
 	/**
 	 * Change what x_axis tracks
+	 * 
 	 * @param field
 	 */
 	public void setXField(String field) {
@@ -231,9 +248,10 @@ public class OlympicScatterplot extends Display {
 		// This line doesn't do anything...
 		m_vis.run("update");
 	}
-	
+
 	/**
 	 * Change what y_axis tracks
+	 * 
 	 * @param field
 	 */
 	public void setYField(String field) {
@@ -241,5 +259,5 @@ public class OlympicScatterplot extends Display {
 		// This line doesn't do anything...
 		m_vis.run("update");
 	}
-	
+
 }
